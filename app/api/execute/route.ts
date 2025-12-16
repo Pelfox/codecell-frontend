95bda15ffba030952e24aff0622781fb0cc36f6a
Aleksey Demchenkov <contact@pelfox.dev>
2025-12-16 15:14:13 +0300

    fix(API/SSE): disable buffering for SSE endpoint

--- app/api/execute/route.ts
+++ app/api/execute/route.ts
@@ -80,6 +80,7 @@ export async function POST(req: NextRequest) {
       'Content-Type': 'text/event-stream',
       'Cache-Control': 'no-cache, no-transform',
       Connection: 'keep-alive',
+      'X-Accel-Buffering': 'no',
     },
   });
 }
